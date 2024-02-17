b64095db791a634d32b06e326d36f66be2c68dc1
ilyas <alexsinotov4710@gmail.com>
2024-02-17 14:41:51 +0500

    changed

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,7 @@ import base64
 
 from django.core.files.base import ContentFile
 from djoser.serializers import UserCreateSerializer
+from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
 
 from user.models import User, Subscribe
@@ -11,17 +12,6 @@ from post.models import (Favorite,
                          RecipeToIngredient,
                          ShopList,
                          Tag)
-
-
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-        return super().to_internal_value(data)
 
 
 class SignUpSerializer(UserCreateSerializer):
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split()
+ALLOWED_HOSTS = ["localhost", '127.0.0.1']
 
 
 INSTALLED_APPS = [
